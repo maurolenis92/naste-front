f89e76f3b35f27aad8267e4c2a4e724f0e5f1df9
Mauricio Lenis <mauricio_lenis@bhd.com.do>
2026-05-05 18:38:13 -0500

    feat: search filter on products temporal design

--- src/app/pages/products/products-view/products-view.component.ts
+++ src/app/pages/products/products-view/products-view.component.ts
@@ -5,11 +5,20 @@ import { ButtonComponent } from '../../../components/button/button.component';
 import { Product } from '../../../shared/models/products.model';
 import { Router } from '@angular/router';
 import { TableComponent, TableData } from '../../../components/table/table.component';
+import { InputComponent } from '../../../components/input/input.component';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products-view',
   standalone: true,
-  imports: [CommonModule, ButtonComponent, TableComponent],
+  imports: [
+    CommonModule,
+    ButtonComponent,
+    TableComponent,
+    InputComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './products-view.component.html',
   styleUrl: './products-view.component.scss',
 })
@@ -33,6 +42,9 @@ export class ProductsViewComponent implements OnInit {
     totalPages: 0,
     currentPage: 1,
   };
+
+  public searchTerm: FormControl = new FormControl('');
+
   public products!: Partial<Product>[];
   private productsService = inject(ProductsService);
   private router = inject(Router);
@@ -42,11 +54,7 @@ export class ProductsViewComponent implements OnInit {
   }
 
   public loadData(page: number): void {
-    this.productsService.getProducts({ page, size: 10 }).subscribe(products => {
-      this.tableData.data = products.data;
-      this.tableData.totalPages = products.pagination.totalPages;
-      this.tableData.currentPage = products.pagination.currentPage;
-    });
+    this._getProducts({ page, size: 10 });
   }
 
   public navigateToCreateProduct(): void {
@@ -56,5 +64,23 @@ export class ProductsViewComponent implements OnInit {
   public navigateToProductDetail(value: unknown): void {
     const id = (value as Record<string, unknown>)['id'] as string;
     this.router.navigate(['dashboard/products', id]);
+  }
+
+  public refresh(): void {
+    this.searchTerm.setValue('');
+    this.loadData(this.tableData.currentPage);
+  }
+
+  public searchBycode(): void {
+    const value = this.searchTerm.value;
+    this._getProducts({ page: 1, size: 10, search: value });
+  }
+
+  private _getProducts<T>(params: T): void {
+    this.productsService.getProducts(params).subscribe(products => {
+      this.tableData.data = products.data;
+      this.tableData.totalPages = products.pagination.totalPages;
+      this.tableData.currentPage = products.pagination.currentPage;
+    });
   }
 }
